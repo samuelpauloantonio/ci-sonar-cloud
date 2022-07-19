2646450d5ef82aa6e1f0a4919b1f4ccf5fc8c62d
samuelpauloantonio <samueldev1997@gmail.com>
2022-07-19 22:22:09 +0100

    add sonar ci

---
+++ src/sum.js
@@ -0,0 +1,22 @@
+function sum (a , b) {
+  return a + b
+}
+
+function dv (a , b) {
+  return a / b
+}
+
+function sb (a , b) {
+  return a - b
+}
+
+function mult (a , b) {
+  return a * b
+}
+
+module.exports = {
+  sum,
+  dv,
+  sb,
+  mult
+}
---
+++ src/sum.test.js
@@ -0,0 +1,30 @@
+
+const {dv, mult, sb, sum} = require('./sum')
+
+
+describe('should be able to make calc', () => {
+  test('should be able to sum two numbers', () => {
+    const result = sum(2,4)
+  
+    expect(result).toEqual(6)
+  })
+  
+  
+  test('should be able to sb two numbers', () => {
+    const result = sb(2,4)
+  
+    expect(result).toEqual(-2)
+  })
+
+  test('should be able to mul two numbers', () => {
+    const result = mult(2,4)
+  
+    expect(result).toEqual(8)
+  })
+
+  test('should be able to mul dv numbers', () => {
+    const result = dv(2,4)
+  
+    expect(result).toEqual(0.5)
+  })
+})
